b5a02b7fc593c089b0b40cd1390a24afe8e31982
Ivan Heng Ze Hao <ivanhengzehao@gmail.com>
2023-08-20 15:00:54 +0800

    Update

--- ToDo/js/ToDo.js
+++ ToDo/js/ToDo.js
@@ -1,17 +1,31 @@
 document.querySelector("#push").onclick = function () {
   if (document.querySelector("#newtask input").value.length == 0) {
-    alert("Please Enter a Task");
+    alert("Please enter a Task");
   } else {
     document.querySelector("#tasks").innerHTML += `
           <div class="task">
               <span id="taskname">
                   ${document.querySelector("#newtask input").value}
               </span>
+              <button class="edit">
+                  <i class="fas fa-edit"></i>
+              </button>
               <button class="delete">
                   <i class="far fa-trash-alt"></i>
               </button>
           </div>
       `;
+
+    var current_tasks = document.querySelectorAll(".edit");
+    for (var i = 0; i < current_tasks.length; i++) {
+      let prevValue = this.previousElementSibling.value;
+      current_tasks[i].onclick = function () {
+        let newValue = prompt("Erdit your task", prevValue);
+        if (newValue != null) {
+          this.previousElementSibling.innerHTML = newValue;
+        }
+      };
+    }
 
     var current_tasks = document.querySelectorAll(".delete");
     for (var i = 0; i < current_tasks.length; i++) {
