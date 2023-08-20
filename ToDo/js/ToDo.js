00ea147620ace26e69a2ff05e27c052a888e1e94
Ivan Heng Ze Hao <ivanhengzehao@gmail.com>
2023-08-20 15:50:28 +0800

    update 4

--- ToDo/js/ToDo.js
+++ ToDo/js/ToDo.js
@@ -1,6 +1,6 @@
 document.querySelector("#push").onclick = function () {
   if (document.querySelector("#newtask input").value.length == 0) {
-    alert("Please enter a Task");
+    alert("Please enter a task");
   } else {
     document.querySelector("#tasks").innerHTML += `
           <div class="task">
